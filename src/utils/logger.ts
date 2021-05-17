3c040a66c0ebebd95ff3b8268fed6715d6792251
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-05-16 21:34:00 -0300

    fix: fix date and time format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -25,7 +25,7 @@ const colors = {
 winston.addColors(colors);
 
 const format: winston.Logform.Format = winston.format.combine(
-  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+  winston.format.timestamp({ format: "DD/MM/YYYY HH:mm:ss" }),
   winston.format.colorize({ all: true }),
   winston.format.printf(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`
